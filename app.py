c6d49535d171ceed80146385f2822206d742174c
youjin2680 <jyj100743@gmail.com>
2023-11-09 15:06:12 +0900

    second commit

--- app.py
+++ app.py
@@ -1,13 +1,23 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash, redirect, url_for, session
 from database import DBhandler
+import hashlib
 import sys
 
 application = Flask(__name__)
+application.config["SECRET_KEY"] = "helloosp"
 DB = DBhandler()
 
 @application.route("/")
 def hello():
     return render_template("index.html")
+
+@application.route("/login")
+def view_login():
+    return render_template("login.html")
+
+@application.route("/signup")
+def view_signup():
+    return render_template("signup.html")
 
 @application.route("/list")
 def view_list():
@@ -37,6 +47,16 @@ def reg_item_submit_post():
     DB.insert_item(data['name'], data, image_file.filename)
     return render_template("submit_item_result.html", data=data, img_path="static/images/{}".format(image_file.filename))
 
+@application.route("/signup_post", methods=['POST'])
+def register_user():
+    data=request.form
+    pw=request.form['pw']
+    pw_hash = hashlib.sha256(pw.encode('utf-8')).hexdigest()
+    if DB.insert_user(data, pw_hash):
+        return render_template("login.html")
+    else:
+        flash("user id already exist!")
+        return render_template("signup.html")
 
 if __name__ == "__main__":
     application.run(host='0.0.0.0', debug=True)
--- database.py
+++ database.py
@@ -23,3 +23,30 @@ class DBhandler:
         self.db.child("item").child(name).set(item_info)
         print(data, img_path)
         return True
+    
+    def insert_user(self, data, pw):
+        user_info ={
+            "id":data['id'],
+            "pw":pw,
+            "nickname":data['nickname']
+        }
+        if self.user_duplicate_check(str(data['id'])):
+            self.db.child("user").push(user_info)
+            print(data)
+            return True
+        else:
+            return False
+    
+    def user_duplicate_check(self, id_string):
+        users = self.db.child("user").get()
+
+        print("users###", users.val())
+        if str(users.val()) == "None": # first registration
+            return True
+        else:
+            for res in users.each():
+                value = res.val()
+
+                if value['id'] == id_string:
+                    return False
+            return True
