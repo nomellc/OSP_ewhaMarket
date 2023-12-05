fa44e72c8457ffcbf5b6e8e3ddc5f5d46af90c7c
dubidubob <sildredel@naver.com>
2023-12-05 22:28:30 +0900

    임시

--- app.py
+++ app.py
@@ -210,16 +210,19 @@ def reg_review():
 
 @application.route('/show_heart/<name>/', methods=['GET'])
 def show_heart(name):
+    print("왜 나에게 이런 일이?1", name)
     my_heart = DB.get_heart_byname(session['id'],name)
     return jsonify({'my_heart': my_heart})
 
 @application.route('/like/<name>/', methods=['POST'])
 def like(name):
+    print("왜 나에게 이런 일이?2", name)
     my_heart = DB.update_heart(session['id'],'Y',name)
     return jsonify({'msg': '좋아요 완료!'})
 
 @application.route('/unlike/<name>/', methods=['POST'])
 def unlike(name):
+    print("왜 나에게 이런 일이?3", name)
     my_heart = DB.update_heart(session['id'],'N',name)
     return jsonify({'msg': '안좋아요 완료!'})
 
--- database.py
+++ database.py
@@ -294,3 +294,26 @@ class DBhandler:
                     })
 
         return matching_items
+
+
+    
+    def get_heart_byname(self, uid, name):
+        hearts = self.db.child("heart").child(uid).get()
+        target_value=""
+        if hearts.val() == None:
+            return target_value
+
+        for res in hearts.each():
+            key_value = res.key()
+
+            if key_value == name:
+                target_value=res.val()
+        return target_value
+
+    def update_heart(self, user_id, isHeart, item):
+        sell_items=self.db.child("item").child(item).get().val()
+        
+        if sell_items is not None:
+            self.db.child("heart").child(user_id).child(item).set(sell_items)
+            
+        return True
